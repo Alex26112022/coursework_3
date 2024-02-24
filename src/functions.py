fdde1b479c3703303eb896943a2a94f96a74a9f2
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-02-24 13:17:36 +0400

    Add date_show and test_date_show.

--- src/functions.py
+++ src/functions.py
@@ -14,3 +14,11 @@ def date_format(date_str: str) -> datetime:
     """ Принимает дату в строковом формате и возвращает в формате даты."""
     date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
     return date_obj
+
+
+def date_show(date_str: str) -> str:
+    """ Принимает дату в строковом формате и возвращает строку в формате
+    '%d.%m.%Y'. """
+    date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+    str_date = datetime.strftime(date_obj, '%d.%m.%Y')
+    return str_date
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -13,3 +13,13 @@ def test_date_format():
     new_date = datetime(2024, 2, 22, 10, 30, 20, 294041)
     """ Проверка перевода даты из str в datetime. """
     assert date_format("2024-02-22T10:30:20.294041") == new_date
+
+
+@pytest.mark.parametrize('date_str, date_obj',
+                         [('2019-07-03T18:35:29.512364', '03.07.2019'),
+                          ('2019-04-04T23:20:05.206878', '04.04.2019'),
+                          ('2018-02-22T00:40:19.984219', '22.02.2018'),
+                          ('2018-06-04T06:59:55.424356', '04.06.2018')])
+def test_date_show(date_str, date_obj):
+    """ Проверка форматирования отображаемой даты. """
+    assert date_show(date_str) == date_obj
